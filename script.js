8d5ba26d8b5da9df28bb08bdc4cc27b48ceb4f9d
OmarMostafa888 <omar88mostafa8@gmail.com>
2025-05-14 12:17:06 +0300

    Equal & Btn functions added

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let inputField = document.getElementById("input-field");
 let acBtn = document.getElementById("ac-btn");
 let delBtn = document.getElementById("del-btn");
+let btns = document.querySelectorAll(".clickable");
+const euqalBtn = document.getElementById("equal-btn");
+
+btns.forEach((e) => {
+  e.addEventListener("click", () => {
+    if (inputField.innerHTML === "0") {
+      inputField.innerHTML = "";
+    }
+    inputField.innerHTML += e.innerHTML;
+  });
+});
+
+function equal(e) {
+  inputField.innerHTML = eval(inputField.innerHTML);
+}
+
+euqalBtn.addEventListener("click", equal);
 
 function deleteAll() {
   inputField.innerHTML = "0";
